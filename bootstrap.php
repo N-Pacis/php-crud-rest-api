2bd4882672bb87212fe475afa6d4dc0cd66b0fe0
n-pacis <pacisnkubito@gmail.com>
2022-07-19 21:59:04 +0200

    debugged the crud and changed database to postgres

--- bootstrap.php
+++ bootstrap.php
@@ -1,9 +1,9 @@
 <?php
 require 'vendor/autoload.php';
-use Dotenv\Dotenv;
+// use Dotenv\Dotenv;
 use Src\System\DatabaseConnector;
 
-$dotenv = new DotEnv(__DIR__);
-$dotenv->load();
+// $dotenv = new DotEnv(__DIR__);
+// $dotenv->load();
 
 $dbConnection = (new DatabaseConnector())->getConnection();
