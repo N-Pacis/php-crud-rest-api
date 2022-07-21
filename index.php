3eb403a973b81ec5064c19c194912de301049028
n-pacis <pacisnkubito@gmail.com>
2022-07-21 22:40:49 +0200

    changed database connection

--- index.php
+++ index.php
@@ -13,6 +13,7 @@ $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $uri = explode('/', $uri);
 
 if ($uri[1] !== 'products') {
+    file_put_contents("php://output", "Hello");
     header("HTTP/1.1 404 Not Found");
     exit();
 }
